8c427f420459399fa01deac3834846f43ecd6032
glavic <glavic@de4125fd-08af-4ef3-add0-b7c1c68fae66>
2012-06-22 17:45:13 +0000

    git-svn-id: https://dblab.cs.toronto.edu/svn/boriscode/STBenchmark/trunk@335 de4125fd-08af-4ef3-add0-b7c1c68fae66

--- src/org/vagabond/benchmark/explgen/SuperfluousMappingGen.java
+++ src/org/vagabond/benchmark/explgen/SuperfluousMappingGen.java
@@ -64,37 +64,37 @@ public class SuperfluousMappingGen extends AbstractExplanationGenerator {
 			TransformationType[] transForMap = scenario.getDoc().getTransForMap(
 					mapType);
 			
-			for (TransformationType t : transForMap) {
-				String targetName = t.getCreates();
-				String cmd = "SELECT tid FROM (SELECT MAPPROV * FROM "
-						+ targetName + ") x " + "WHERE trans_prov LIKE '%"
-						+ mapping + "%' limit 1";
-				try {
-					ResultSet rs = ConnectionManager.getInstance().execQuery(
-							cmd);
-					String tid = null;
-					if (rs.next()) {
-						tid = rs.getString("tid");
-					}
-					if (tid != null) {
-						ISingleMarker errorMarker = MarkerFactory
-								.newAttrMarker(targetName, tid, 0);
-						err = new SuperflousMappingError(
-								(IAttributeValueMarker) errorMarker);
-						gen.setExpl(err);
-						gen.getExpl().addMapSE(mapType);
-						gen.getExpl().setTransSE(
-								CollectionUtils.makeList(transForMap));
-						break;
-					}
-				} catch (Exception e) {
-					LoggerUtil.logException(e, log);
-				}
-			}
-			for (TransformationType t: transForMap)
-				gen.computeSideEffects(t.getCreates(), mappingSet);
-			
-			e.addExplanation(gen.getExpl());
+//			for (TransformationType t : transForMap) {
+//				String targetName = t.getCreates();
+//				String cmd = "SELECT tid FROM (SELECT MAPPROV * FROM "
+//						+ targetName + ") x " + "WHERE trans_prov LIKE '%"
+//						+ mapping + "%' limit 1";
+//				try {
+//					ResultSet rs = ConnectionManager.getInstance().execQuery(
+//							cmd);
+//					String tid = null;
+//					if (rs.next()) {
+//						tid = rs.getString("tid");
+//					}
+//					if (tid != null) {
+//						ISingleMarker errorMarker = MarkerFactory
+//								.newAttrMarker(targetName, tid, 0);
+//						err = new SuperflousMappingError(
+//								(IAttributeValueMarker) errorMarker);
+//						gen.setExpl(err);
+//						gen.getExpl().addMapSE(mapType);
+//						gen.getExpl().setTransSE(
+//								CollectionUtils.makeList(transForMap));
+//						break;
+//					}
+//				} catch (Exception e) {
+//					LoggerUtil.logException(e, log);
+//				}
+//			}
+//			for (TransformationType t: transForMap)
+//				gen.computeSideEffects(t.getCreates(), mappingSet);
+//			
+//			e.addExplanation(gen.getExpl());
 		}
 	}
 
